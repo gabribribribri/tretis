82fed4e85441fecd62f78c249694a4e2b51c32d8
gabribribribri <fuelanemo149@gmail.com>
2024-06-11 22:59:24 +0200

    Ok now it works !

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,10 @@
 // #include <SFML/Graphics/RenderWindow.hpp>
 // #include <SFML/Window.hpp>
 // #include <SFML/Graphics.hpp>
-#include <iostream>
 #include <print>
 
 int main() {
-    std::println("Woaw j'ai pas le droit de faire ça du tout");
+    std::println("sub to my balls");
 }
 
 
