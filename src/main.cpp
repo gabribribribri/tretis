4e7d3a824102aee160453fdbc83f4df6de742bc3
gabribribribri <fuelanemo149@gmail.com>
2024-06-11 15:23:28 +0200

    remade everything

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,17 @@
-import my_module;
+/// ***
+/// Petit point culture :
+/// On habite avec un coeur plein un monde vide - Chateaubriand
+/// Mieux une tête bien faite qu'un tête bien pleine - Montaigne
+/// ***
 
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Graphics.hpp>
+// #include <SFML/Graphics/RenderWindow.hpp>
+// #include <SFML/Window.hpp>
+// #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <print>
 
 int main() {
-    int i = my_module::bonsoir();
-    std::cout << i << "\n";
+    std::println("Woaw j'ai pas le droit de faire ça du tout");
 }
 
 
-// int main()
-// {
-//     // Create a window with a size of 800x600 pixels and titled "Basic SFML Window"
-//     sf::RenderWindow window(sf::VideoMode(900, 600), "Basic SFML Window");
-    
-
-//     // Main loop
-//     while (window.isOpen())
-//     {
-//         // Process events
-//         sf::Event event;
-//         while (window.pollEvent(event))
-//         {
-//             // Close window: exit
-//             if (event.type == sf::Event::Closed)
-//                 window.close();
-//         }
-
-//         // Clear screen
-//         window.clear();
-
-//         // Draw everything here...
-
-//         // Update the window
-//         window.display();
-
-//         // wait
-//         std::this_thread::sleep_for(std::chrono::milliseconds(50));
-//     }
-
-//     return 0;
-// }
--- src/mod.impl.cpp
+++
@@ -1,7 +0,0 @@
-module my_module;
-
-namespace my_module {
-  int bonsoir(void) {
-    return 1000;
-  }
-}
