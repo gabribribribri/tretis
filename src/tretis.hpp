cb28c9b273ee6dcd9702ce756c433d944f182ac8
gabribribribri <fuelanemo149@gmail.com>
2024-09-28 16:46:42 +0200

    thing

--- src/tretis.hpp
+++ src/tretis.hpp
@@ -7,7 +7,6 @@
 #include <array>
 #include <iostream>
 
-
 const int GRID_HEIGHT = 20;
 const int GRID_WIDTH = 10;
 const int CELL_SIZE = 30;
@@ -17,17 +16,16 @@ public:
     std::array<sf::RectangleShape, GRID_HEIGHT * GRID_WIDTH> grid;
     sf::RenderWindow render_window { sf::VideoMode(800, 1200), "Tretis" };
 
-    static Tretis& Get() {
-        return tretis;
-    }
+    static Tretis& Get() { return tretis; }
 
     void gameloop() {
         for (;;) {
-            for (int y = 0; y < GRID_HEIGHT ; y++) {
+            render_window.clear(sf::Color::Black);
+            for (int y = 0; y < GRID_HEIGHT; y++) {
                 for (int x = 0; x < GRID_WIDTH; x++) {
-                    sf::RectangleShape& cell = grid[y*GRID_WIDTH+x];
-                    cell = sf::RectangleShape(sf::Vector2f(50, 50));
-                    cell.setPosition(sf::Vector2f(x*CELL_SIZE, y*CELL_SIZE));
+                    sf::RectangleShape& cell = grid_at(x, y);
+                    cell.setPosition(
+                        sf::Vector2f(x * CELL_SIZE, y * CELL_SIZE));
                     cell.setFillColor(sf::Color::Blue);
                     render_window.draw(cell);
                 }
@@ -36,15 +34,23 @@ public:
         }
     }
 
+    sf::RectangleShape& grid_at(int x, int y) {
+        return grid[y * GRID_WIDTH + x];
+    }
+
 private:
     ~Tretis() = default;
-    Tretis() = default;
+    Tretis() {
+        for (auto& cell : grid) {
+            cell = sf::RectangleShape(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+        }
+    };
     Tretis(const Tretis&) = delete;
     Tretis(Tretis&&) = delete;
     Tretis& operator=(const Tretis&) = delete;
+
     static Tretis tretis;
 };
-
 
 // shut up I hate ODR
 Tretis Tretis::tretis;
